bf8d48e291af47f972b33fb75377e41fb3f2a909
takanashi shoya <220576@jc-21.jp>
2023-05-30 16:33:51 +0900

    くぁｗせｄｒｆｔｇｙふじこｌｐ

--- Direct3D.cpp
+++ Direct3D.cpp
@@ -117,9 +117,9 @@ void Direct3D::InitShader()
 
 	//ラスタライザ作成
 	D3D11_RASTERIZER_DESC rdc = {};
-	rdc.CullMode = D3D11_CULL_BACK;
-	rdc.FillMode = D3D11_FILL_SOLID;
-	rdc.FrontCounterClockwise = FALSE;
+	rdc.CullMode = D3D11_CULL_NONE;
+	rdc.FillMode = D3D11_FILL_WIREFRAME;
+	rdc.FrontCounterClockwise = TRUE;
 	pDevice->CreateRasterizerState(&rdc, &pRasterizerState);
 
 	//それぞれをデバイスコンテキストにセット
