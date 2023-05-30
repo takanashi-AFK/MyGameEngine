edc86fe37be830e36572a529aa662ce8221da236
takanashi shoya <220576@jc-21.jp>
2023-05-30 14:38:36 +0900

    プロジェクト ファイルを追加します。

---
+++ Direct3D.h
@@ -0,0 +1,28 @@
+#pragma once
+
+#include <d3d11.h>
+
+//リンカ
+#pragma comment(lib, "d3d11.lib")
+#pragma comment(lib, "d3dcompiler.lib")
+
+namespace Direct3D
+{
+	extern ID3D11Device* pDevice;		//デバイス
+	extern 	ID3D11DeviceContext* pContext;		//デバイスコンテキスト
+
+	//初期化
+	void Initialize(int winW, int winH, HWND hWnd);
+
+	//シェーダー準備
+	void InitShader();
+
+	//描画開始
+	void BeginDraw();
+
+	//描画終了
+	void EndDraw();
+
+	//解放
+	void Release();
+};
