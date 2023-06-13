e4b3f5e8fc5b1f6d30e6f4388c0ab538e367595a
takanashi shoya <220576@jc-21.jp>
2023-06-13 14:45:13 +0900

    mendokusainode

--- Dice.h
+++ Dice.h
@@ -3,28 +3,13 @@
 #include <DirectXMath.h>
 #include "Direct3D.h"
 #include"Texture.h"
+#include"Quad.h"
 
 using namespace DirectX;
 
-//コンスタントバッファ
-struct CONSTANT_BUFFER
-{
-	XMMATRIX	matWVP;
-};
 
-//頂点情報
-struct VERTEX
+class Dice :public Quad
 {
-	XMVECTOR position;
-	XMVECTOR uv;
-};
-
-class Dice
-{
-	ID3D11Buffer* pVertexBuffer_;	//頂点バッファ
-	ID3D11Buffer* pIndexBuffer_;
-	ID3D11Buffer* pConstantBuffer_;	//コンスタントバッファ
-	Texture* pTexture_;
 
 public:
 	Dice();
--- Quad.h
+++ Quad.h
@@ -1,36 +1,38 @@
-//#pragma once
-//
-//#include <DirectXMath.h>
-//#include "Direct3D.h"
-//#include"Texture.h"
-//
-//using namespace DirectX;
-//
-////コンスタントバッファ
-//struct CONSTANT_BUFFER
-//{
-//	XMMATRIX	matWVP;
-//};
-//
-////頂点情報
-//struct VERTEX
-//{
-//	XMVECTOR position;
-//	XMVECTOR uv;
-//};
-//
-//class Quad
-//{
-//	ID3D11Buffer* pVertexBuffer_;	//頂点バッファ
-//	ID3D11Buffer* pIndexBuffer_;
-//	ID3D11Buffer* pConstantBuffer_;	//コンスタントバッファ
-//	Texture* pTexture_;
-//
-//public:
-//	Quad();
-//	~Quad();
-//	HRESULT Initialize();
-//	void Draw(XMMATRIX& worldMatrix);
-//	void Release();
-//};
-//
+#pragma once
+
+#include <DirectXMath.h>
+#include "Direct3D.h"
+#include"Texture.h"
+
+using namespace DirectX;
+
+//コンスタントバッファ
+struct CONSTANT_BUFFER
+{
+	XMMATRIX	matWVP;
+};
+
+//頂点情報
+struct VERTEX
+{
+	XMVECTOR position;
+	XMVECTOR uv;
+};
+
+class Quad
+{
+protected:
+	ID3D11Buffer* pVertexBuffer_;	//頂点バッファ
+	ID3D11Buffer* pIndexBuffer_;
+	ID3D11Buffer* pConstantBuffer_;	//コンスタントバッファ
+	Texture* pTexture_;
+
+public:
+	Quad();
+	~Quad();
+	HRESULT Initialize();
+	void Draw(XMMATRIX& worldMatrix);
+	void Release();
+	HRESULT BufferMaker(VERTEX* _v, int* _index,int _vNum,int _iNum);
+	void BufferSetter(XMMATRIX& worldMatrix);
+};
