10d8de2405b3f2ad62fe56a43263848bbc0bb381
takanashi shoya <220576@jc-21.jp>
2023-06-23 10:08:34 +0900

    FBXクラス作成

---
+++ Fbx.h
@@ -0,0 +1,21 @@
+#pragma once
+
+#include <d3d11.h>
+#include <fbxsdk.h>
+#include <string>
+#include "Transform.h"
+
+#pragma comment(lib, "LibFbxSDK-Md.lib")
+#pragma comment(lib, "LibXml2-Md.lib")
+#pragma comment(lib, "zlib-Md.lib")
+
+class Fbx
+{
+
+public:
+
+	Fbx();
+	HRESULT Load(std::string fileName);
+	void    Draw(Transform& transform);
+	void    Release();
+};
